4c8f778dca1213faaa63ab3b1bdff9008c319e2c
13xforever <ilya.veselov@gmail.com>
2010-03-30 00:20:28 +0600

    New board layout that I made today :D
    Actually, it's my first relatively deep dive into WPF.

--- Triple Triad .NET/BoardWindow.xaml.cs
+++ Triple Triad .NET/BoardWindow.xaml.cs
@@ -12,14 +12,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TripleTriad
+namespace Board
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for BoardWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class BoardWindow : Window
 	{
-		public MainWindow()
+		public BoardWindow()
 		{
 			InitializeComponent();
 		}
