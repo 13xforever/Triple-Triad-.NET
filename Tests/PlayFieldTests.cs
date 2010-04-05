ab4b381e6f5051da6a0d38048be47b59f5fce904
13xforever <ilya.veselov@gmail.com>
2010-04-05 17:58:19 +0600

    Split test cases

---
+++ Tests/PlayFieldTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using TripleTriad.Solver;
+
+namespace Tests
+{
+	[TestFixture]
+	public class PlayFieldTests
+	{
+		[Test]
+		public void PlayField()
+		{
+			var field = new PlayField();
+			field.cell[0, 0].card = new PlayCard(CardInfo.CardPool["Squall"]);
+
+			Assert.That(field.cell.Length, Is.EqualTo(3 * 3));
+			Assert.That(field.cell.GetLength(0), Is.EqualTo(3));
+
+			Assert.That(field.cell[0, 0].left, Is.EqualTo(10));
+			Assert.That(field.cell[0, 0].up, Is.EqualTo(10));
+			Assert.That(field.cell[0, 0].right, Is.EqualTo(0));
+			Assert.That(field.cell[0, 0].down, Is.EqualTo(0));
+
+			PlayField field2 = field.Clone();
+			Assert.That(field.cell[0, 0].card.cardInfo, Is.EqualTo(field2.cell[0, 0].card.cardInfo));
+			Assert.That(field.cell[0, 0].card.isBlue, Is.EqualTo(field2.cell[0, 0].card.isBlue));
+
+			field2.cell[0, 0].card.Flip();
+			Assert.That(field.cell[0, 0].card.isBlue, Is.Not.EqualTo(field2.cell[0, 0].card.isBlue));
+
+			field2.cell[0, 0].card = new PlayCard(CardInfo.CardPool["Bomb"]);
+			Assert.That(field.cell[0, 0].card.cardInfo, Is.Not.EqualTo(field2.cell[0, 0].card.cardInfo));
+		}
+	}
+}
---
+++ Tests/StateTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using TripleTriad.Solver;
+
+namespace Tests
+{
+	[TestFixture]
+	public class StateTests
+	{
+		[Test]
+		public void StateConstruction()
+		{
+			var blueHand = new PlayHand(new[]
+			                            	{
+			                            		new PlayCard(CardInfo.CardPool["Squall"]),
+			                            		new PlayCard(CardInfo.CardPool["Krysta"]),
+			                            		new PlayCard(CardInfo.CardPool["Wendigo"]),
+			                            		new PlayCard(CardInfo.CardPool["Behemoth"]),
+			                            		new PlayCard(CardInfo.CardPool["Tonberry"]),
+			                            	}, true);
+			var redHand = new PlayHand(new[]
+			                           	{
+			                           		new PlayCard(CardInfo.CardPool["Torama"]),
+			                           		new PlayCard(CardInfo.CardPool["Gerogero"]),
+			                           		new PlayCard(CardInfo.CardPool["Buel"]),
+			                           		new PlayCard(CardInfo.CardPool["Bomb"]),
+			                           		new PlayCard(CardInfo.CardPool["Diablos"]),
+			                           	}, false);
+			var state = new State(new PlayField(), RuleModifier.None, blueHand, redHand, true);
+
+			Assert.That(state.bluePoints, Is.EqualTo(5));
+			state.bluePoints--;
+			Assert.That(state.bluePoints, Is.EqualTo(4));
+			State clone = state.Clone();
+			Assert.That(clone.bluePoints, Is.EqualTo(4));
+			clone.bluePoints--;
+			Assert.That(state.bluePoints, Is.EqualTo(4));
+			Assert.That(clone.bluePoints, Is.EqualTo(3));
+		}
+	}
+}
